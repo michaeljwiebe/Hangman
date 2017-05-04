836fb265e9f744f12a810295e4b5f2b32beaad49
Michael J. Wiebe <wiebe.michael.j@gmail.com>
2017-05-04 16:32:59 -0400

    added loadRounds

--- scripts.js
+++ scripts.js
@@ -2,7 +2,6 @@
 
 // A single page app allowing the user to play a simple game of Wheel of Fortune. Use JavaScript to give the user a word to guess letter by letter. Will the user lose points as they guess wrong? Will they try to beat the clock? Do they only get 3 guesses? You decide! Make it clear to the user what their goal is. The app should be styled with simple but modern design trends and fun for the user. Remember keep your user engaged.
 
-// word to guess letter by letter, hints given for each word, several rounds available
 // Make your game multiplayer. Players should be objects.
 // Without using a library, construct a wheel that spins and has values, wheel of fortune style. When the wheel lands on a value, the user should get that many points upon answering the word correctly (or go bankrupt).
 
@@ -16,12 +15,9 @@ var hintDisplay = document.getElementsByClassName("hint")[0];
 var gameBoard = document.getElementsByClassName("game-board")[0];
 var wordArrayBeingGuessed;
 
-var round1 = new Round("sunshine", "brightnass");
-var round2 = new Round("JavaScript", "adaptive, interactive!");
-var round3 = new Round("gyro", "Kostas snack");
-var round4 = new Round("Amsterdam", "City across the pond");
 
 startBtn.addEventListener("click", function(){
+	loadRounds();
 	initGame();
 });
 nextRound.addEventListener("click", function(){
@@ -37,26 +33,37 @@ function Round(word, hint){
 	rounds.push(this);
 }
 
-function initGame(){
-	var random = Math.floor(Math.random()*(rounds.length));
-	gameBoard.innerHTML = "";
-	guesses.innerHTML = "";
-	var currentRound = rounds.splice(random, 1)[0];
-	var roundWord = currentRound.word;
-	var roundHint = currentRound.hint;
-	hintDisplay.innerText = "The hint for this word is: " + roundHint;
-	wordArrayBeingGuessed = roundWord.split("");
-	wordArrayBeingGuessed.forEach(function(letter, index){
-		var underscoreDiv = document.createElement("div");
-		underscoreDiv.classList.add("underscore", "underscore" + index);
-		underscoreDiv.innerHTML = "___";
-		var letterDiv = document.createElement("div");
-		letterDiv.classList.add("letter", "letter" + index);
-		letterDiv.innerHTML = letter;
+function loadRounds(){
+	var round1 = new Round("sunshine", "brightnass");
+	var round2 = new Round("JavaScript", "adaptive, interactive!");
+	var round3 = new Round("gyro", "Kostas snack");
+	var round4 = new Round("Amsterdam", "City across the pond");
+};
 
-		gameBoard.append(underscoreDiv);
-		gameBoard.append(letterDiv);
-	})
+function initGame(){
+	if (rounds.length > 0){
+		var random = Math.floor(Math.random()*(rounds.length));
+		gameBoard.innerHTML = "";
+		guesses.innerHTML = "";
+		var currentRound = rounds.splice(random, 1)[0];
+		var roundWord = currentRound.word;
+		var roundHint = currentRound.hint;
+		hintDisplay.innerText = "The hint for this word is: " + roundHint;
+		wordArrayBeingGuessed = roundWord.split("");
+		wordArrayBeingGuessed.forEach(function(letter, index){
+			var underscoreDiv = document.createElement("div");
+			underscoreDiv.classList.add("underscore", "underscore" + index);
+			underscoreDiv.innerHTML = "___";
+			var letterDiv = document.createElement("div");
+			letterDiv.classList.add("letter", "letter" + index);
+			letterDiv.innerHTML = letter;
+
+			gameBoard.append(underscoreDiv);
+			gameBoard.append(letterDiv);
+		});
+	} else {
+		alert("The game is finished! You win!");
+	}
 }
 
 function guessLetter(letter){
