10ad5b114d907e440967b6961f50a996dbc8203d
Michael J. Wiebe <wiebe.michael.j@gmail.com>
2017-08-01 17:23:56 -0400

    updated rounds

--- scripts.js
+++ scripts.js
@@ -22,31 +22,25 @@ currentPlayer = players[0];
 players[0].currentPlayer = true;
 initGame();
 
-startBtn.addEventListener("click", function(){
+startBtn.addEventListener("click", function() {
 	loadRounds();
 	currentPlayer = players[0];
 	initGame();
 });
-nextRound.addEventListener("click", function(){
+nextRound.addEventListener("click", function() {
 	initGame();
 });
-document.addEventListener("keypress", function(){
+document.addEventListener("keypress", function() {
 	currentPlayer.guessLetter(event.key);
 });
 
-function Round(word, hint){
-	this.word = word,
-	this.hint = hint
+function Round(word, hint) {
+	(this.word = word), (this.hint = hint);
 	rounds.push(this);
 }
 
-function Player(name, num){
-	this.name = name,
-	this.score = 0,
-	this.guessLetter = guessLetter,
-	this.playerNum = num,
-	this.currentPlayer = false,
-	this.ScoreBoard = updateScoreBoard
+function Player(name, num) {
+	(this.name = name), (this.score = 0), (this.guessLetter = guessLetter), (this.playerNum = num), (this.currentPlayer = false), (this.ScoreBoard = updateScoreBoard);
 
 	players.push(this);
 
@@ -58,7 +52,7 @@ function Player(name, num){
 
 	playerScore.classList.add("playerScoreBoard");
 	playerDiv.classList.add("flex");
-	playerScoreDiv.classList.add("scores")
+	playerScoreDiv.classList.add("scores");
 	playerName.innerHTML += this.name + ": ";
 	playerScore.innerHTML += this.score;
 
@@ -67,55 +61,68 @@ function Player(name, num){
 	playerScoreDiv.append(playerScore);
 	playerDiv.append(playerScoreDiv);
 
-	function updateScoreBoard(){
+	function updateScoreBoard() {
 		var individualScores = document.getElementsByClassName("playerScoreBoard");
 
-		players.forEach(function(player, index){
+		players.forEach(function(player, index) {
 			individualScores[index].innerHTML = player.score;
-		})
+		});
 	}
 
-	function guessLetter(letter){
+	function guessLetter(letter) {
 		letter = letter.toLowerCase();
 		guesses.innerHTML += " " + letter;
 		var joinedWord = roundWord.split(" ").join("");
 		var correct = 0;
 
-		if (playerCorrectLetters.indexOf(letter) > -1){
-			announcements.innerHTML = "The letter " + letter + " has already been guessed. Please guess again.";
+		if (playerCorrectLetters.indexOf(letter) > -1) {
+			announcements.innerHTML =
+				"The letter " + letter + " has already been guessed. Please guess again.";
 		} else {
-			for(let i = 0; i < joinedWord.length; i++){
-				if (letter === joinedWord[i].toLowerCase()){
+			for (let i = 0; i < joinedWord.length; i++) {
+				if (letter === joinedWord[i].toLowerCase()) {
 					correct += 1;
 					document.getElementsByClassName("underscore")[i].style.display = "none";
 					document.getElementsByClassName("letter")[i].style.display = "block";
 					currentPlayer.score += 100;
 				}
-				if (i === joinedWord.length - 1 && correct > 0){
-					playerCorrectLetters.push(letter)
+				if (i === joinedWord.length - 1 && correct > 0) {
+					playerCorrectLetters.push(letter);
 				}
 			}
 		}
 
-		if (correct > 0){
-			announcements.innerHTML = "The letter " + letter + " was in this word " + correct + " times. You get " + correct * 100 + " points.";
-		} else if(playerCorrectLetters.indexOf(letter) === -1) {
-			announcements.innerHTML = "Your guess of the letter " + letter + " was incorrect. You lose 200 points.";
+		if (correct > 0) {
+			announcements.innerHTML =
+				"The letter " +
+				letter +
+				" was in this word " +
+				correct +
+				" times. You get " +
+				correct * 100 +
+				" points.";
+		} else if (playerCorrectLetters.indexOf(letter) === -1) {
+			announcements.innerHTML =
+				"Your guess of the letter " + letter + " was incorrect. You lose 200 points.";
 			this.score -= 200;
-			setTimeout(function(){nextPlayer()}, 3000);
-		} 
-		if (playerCorrectLetters.length === roundCorrectLetters.length){
+			setTimeout(function() {
+				nextPlayer();
+			}, 3000);
+		}
+		if (playerCorrectLetters.length === roundCorrectLetters.length) {
 			announcements.innerHTML = "You win this round! +1000 points";
 			this.score += 1000;
-			setTimeout(function(){initGame()}, 3000);
+			setTimeout(function() {
+				initGame();
+			}, 3000);
 		}
 		updateScoreBoard();
 	}
 }
 
-function nextPlayer(){
-	for(let i = 0; i < players.length; i++){
-		if (players[i].currentPlayer === true){
+function nextPlayer() {
+	for (let i = 0; i < players.length; i++) {
+		if (players[i].currentPlayer === true) {
 			players[i].currentPlayer = false;
 		} else {
 			players[i].currentPlayer = true;
@@ -125,46 +132,48 @@ function nextPlayer(){
 	announcements.innerHTML = currentPlayer.name + ", guess a letter.";
 }
 
-function loadRounds(){
-	var round1 = new Round("sunshine", "brightnass");
-	var round2 = new Round("JavaScript", "adaptive, interactive!");
-	var round3 = new Round("gyro", "Kostas snack");
+function loadRounds() {
+	var round1 = new Round("Sunshine", "Brightness");
+	var round2 = new Round("JavaScript", "It's adaptive, it's interactive!");
+	var round3 = new Round("Gyro", "Food truck meal");
 	var round4 = new Round("Amsterdam", "City across the pond");
-	var round5 = new Round("rotweiler", "Scary dog");
+	var round5 = new Round("Rotweiler", "Scary dog");
 	var round6 = new Round("Gary Fisher", "fast bike");
 	var round7 = new Round("New Zealand", "Maori country");
-	var round8 = new Round("HTML + CSS", "WebDev languages");
-};
+	var round8 = new Round("HTML + CSS", "Web Development languages");
+	var round9 = new Round("Philadelphia", "An awesome east-coast city");
+	var round10 = new Round("West Philly", "Best neighborhood");
+}
 
-function initGame(){
+function initGame() {
 	roundCorrectLetters = [];
 	playerCorrectLetters = [];
 	nextPlayer();
 
-	if (rounds.length > 0){
-		var random = Math.floor(Math.random()*(rounds.length));
+	if (rounds.length > 0) {
+		var random = Math.floor(Math.random() * rounds.length);
 		var currentRound = rounds.splice(random, 1)[0];
 		var roundHint = currentRound.hint;
 		var index = 0;
-		
+
 		roundWord = currentRound.word;
 		gameBoard.innerHTML = "";
 		guesses.innerHTML = "";
 		hintDisplay.innerText = "The hint for this word is: " + roundHint;
-		roundWord.split("").forEach(function(letter){
-			if (roundCorrectLetters.indexOf(letter) === -1 && letter != " "){
+		roundWord.split("").forEach(function(letter) {
+			if (roundCorrectLetters.indexOf(letter) === -1 && letter != " ") {
 				roundCorrectLetters.push(letter);
 			}
-		})
-		if (roundWord.indexOf(" ") > -1){
+		});
+		if (roundWord.indexOf(" ") > -1) {
 			var wordArray = roundWord.split(" ");
-			wordArray.forEach(function(word){
+			wordArray.forEach(function(word) {
 				var letterArrays = word.split("");
 				displayBlankDivs(letterArrays);
 				var spaceDiv = document.createElement("div");
 				spaceDiv.classList.add("space");
 				gameBoard.append(spaceDiv);
-			})
+			});
 		} else {
 			var letterArray = roundWord.split("");
 			displayBlankDivs(letterArray);
@@ -173,8 +182,8 @@ function initGame(){
 		alert("The game is finished!");
 	}
 
-	function displayBlankDivs(letterArray){
-		letterArray.forEach(function(letter){
+	function displayBlankDivs(letterArray) {
+		letterArray.forEach(function(letter) {
 			var underscoreDiv = document.createElement("div");
 			underscoreDiv.classList.add("underscore", "underscore" + index);
 			underscoreDiv.innerHTML = "";
@@ -185,14 +194,6 @@ function initGame(){
 			gameBoard.append(underscoreDiv);
 			gameBoard.append(letterDiv);
 			index += 1;
-		})
+		});
 	}
 }
-
-
-
-
-
-
-
-
